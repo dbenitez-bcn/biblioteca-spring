b85b8bd71b5b570d458a9c14a4c267ade5a25bfd
dbenitez-bcn <dbr.bcn@gmail.com>
2020-08-09 17:45:07 +0200

    adds movie entity

--- src/main/java/com/example/biblioteca/app/multimedia/infrastructure/postgresql/entities/MovieEntity.java
+++ src/main/java/com/example/biblioteca/app/multimedia/infrastructure/postgresql/entities/MovieEntity.java
@@ -1,4 +1,19 @@
 package com.example.biblioteca.app.multimedia.infrastructure.postgresql.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class MovieEntity {
+    @Id
+    private Long id;
+    private String name;
+    private Integer year;
 }
