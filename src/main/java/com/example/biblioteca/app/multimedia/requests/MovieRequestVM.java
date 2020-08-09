a278eb4b7ac2429df39ec6b32f99dacb1292cca0
dbenitez-bcn <dbr.bcn@gmail.com>
2020-08-09 12:33:40 +0200

    creates vm for movie controller

---
+++ src/main/java/com/example/biblioteca/app/multimedia/requests/MovieRequestVM.java
@@ -0,0 +1,9 @@
+package com.example.biblioteca.app.multimedia.requests;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class MovieRequestVM {
+    public final String name;
+    public final int year;
+}
---
+++ src/main/java/com/example/biblioteca/app/multimedia/responses/MovieResponseVM.java
@@ -0,0 +1,12 @@
+package com.example.biblioteca.app.multimedia.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode
+@AllArgsConstructor
+public class MovieResponseVM {
+    public final String id;
+    public final String name;
+    public final int year;
+}
