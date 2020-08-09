5e08da22607fd08468802be2f3cf36f53f2a2ec0
dbenitez-bcn <dbr.bcn@gmail.com>
2020-08-09 12:33:12 +0200

    adds getter annotation

--- src/main/java/com/example/biblioteca/modules/multimedia/movies/domain/valueObjects/MovieName.java
+++ src/main/java/com/example/biblioteca/modules/multimedia/movies/domain/valueObjects/MovieName.java
@@ -2,10 +2,12 @@ package com.example.biblioteca.modules.multimedia.movies.domain.valueObjects;
 
 import com.example.biblioteca.modules.multimedia.movies.domain.exceptions.InvalidNameForMovie;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
 
+@Getter
 @EqualsAndHashCode
 public class MovieName {
-    private String value;
+    private final String value;
 
     public MovieName(String value) {
         if (value == null || value.isEmpty() || value.trim().length() == 0) {
--- src/main/java/com/example/biblioteca/modules/multimedia/movies/domain/valueObjects/MovieYear.java
+++ src/main/java/com/example/biblioteca/modules/multimedia/movies/domain/valueObjects/MovieYear.java
@@ -2,10 +2,12 @@ package com.example.biblioteca.modules.multimedia.movies.domain.valueObjects;
 
 import com.example.biblioteca.modules.multimedia.movies.domain.exceptions.InvalidYearForMovie;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
 
+@Getter
 @EqualsAndHashCode
 public class MovieYear {
-    private int value;
+    private final int value;
 
     public MovieYear(int value) {
         if (value < 1888) {
