c4ad444ed4dd1bc3f1d1c5e2a915f397afa8dba5
dbenitez-bcn <dbr.bcn@gmail.com>
2020-09-21 22:34:30 +0200

    creates Error details

---
+++ src/main/java/com/example/biblioteca/app/multimedia/application/exceptions/ErrorDetails.java
@@ -0,0 +1,8 @@
+package com.example.biblioteca.app.multimedia.application.exceptions;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class ErrorDetails {
+    public final String message;
+}
