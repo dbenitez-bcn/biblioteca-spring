3249bb59932ed806ac33c90fbeb3976b576420ab
dbenitez-bcn <dbr.bcn@gmail.com>
2020-09-26 17:33:01 +0200

    adds e2e tests

---
+++ src/test/java/com/example/biblioteca/app/multimedia/application/controllers/MovieControllerE2E.java
@@ -0,0 +1,44 @@
+package com.example.biblioteca.app.multimedia.application.controllers;
+
+import com.example.biblioteca.ApplicationTestCase;
+import com.example.biblioteca.modules.multimedia.movies.domain.aggregates.Movie;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+
+import static com.example.biblioteca.modules.multimedia.movies.domain.fixtures.MovieFixture.randomMovie;
+
+class MovieControllerE2E extends ApplicationTestCase {
+
+    @Test
+    void happyPath_userCreatesAMovieThatCouldFind() throws Exception {
+        Movie movie = randomMovie();
+
+        // User doesn't find the movie is looking for
+        assertRequest(
+                HttpMethod.GET.toString(),
+                "/v1/movie/" + movie.getId().toString(),
+                HttpStatus.NOT_FOUND.value()
+        );
+
+        // User creates a movie
+        JSONObject movieBody = new JSONObject();
+        movieBody.put("name", movie.getName().getValue());
+        movieBody.put("year", movie.getYear().getValue());
+
+        assertRequestWithBody(
+                HttpMethod.POST.toString(),
+                "/v1/movie",
+                movieBody.toString(),
+                HttpStatus.CREATED.value()
+        );
+
+        // User search for all movies
+        assertRequest(
+                HttpMethod.GET.toString(),
+                "/v1/movie/" + movie.getId().toString(),
+                HttpStatus.OK.value()
+        );
+    }
+}
