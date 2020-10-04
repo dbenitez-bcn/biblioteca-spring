45e0079e4f1fe979a4a8ddcf0c06a20dc8049a5e
dbenitez-bcn <dbr.bcn@gmail.com>
2020-10-04 02:18:05 +0200

    adds exception handlers

--- src/main/java/com/example/biblioteca/app/shared/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/example/biblioteca/app/shared/exceptions/GlobalExceptionHandler.java
@@ -1,5 +1,8 @@
 package com.example.biblioteca.app.shared.exceptions;
 
+import com.example.biblioteca.modules.accounts.domain.exceptions.EmailAlreadyInUse;
+import com.example.biblioteca.modules.accounts.domain.exceptions.InvalidEmailAddress;
+import com.example.biblioteca.modules.accounts.domain.exceptions.InvalidPasswordFormat;
 import com.example.biblioteca.modules.multimedia.movies.domain.exceptions.InvalidNameForMovie;
 import com.example.biblioteca.modules.multimedia.movies.domain.exceptions.InvalidYearForMovie;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +20,24 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(InvalidYearForMovie.class)
     public ResponseEntity<ErrorDetails> invalidYearForMovie() {
         ErrorDetails errorDetails = new ErrorDetails("Invalid movie year");
+        return ResponseEntity.status(422).body(errorDetails);
+    }
+
+    @ExceptionHandler(EmailAlreadyInUse.class)
+    public ResponseEntity<ErrorDetails> emailAlreadyInUse() {
+        ErrorDetails errorDetails = new ErrorDetails("Email is already in use");
+        return ResponseEntity.status(422).body(errorDetails);
+    }
+
+    @ExceptionHandler(InvalidEmailAddress.class)
+    public ResponseEntity<ErrorDetails> invalidEmailAddress() {
+        ErrorDetails errorDetails = new ErrorDetails("Invalid email address");
+        return ResponseEntity.status(422).body(errorDetails);
+    }
+
+    @ExceptionHandler(InvalidPasswordFormat.class)
+    public ResponseEntity<ErrorDetails> invalidPasswordFormat(InvalidPasswordFormat exception) {
+        ErrorDetails errorDetails = new ErrorDetails("Invalid password format: " + exception.getMessage());
         return ResponseEntity.status(422).body(errorDetails);
     }
 }
--- src/test/java/com/example/biblioteca/app/shared/exceptions/GlobalExceptionHandlerTest.java
+++ src/test/java/com/example/biblioteca/app/shared/exceptions/GlobalExceptionHandlerTest.java
@@ -1,5 +1,6 @@
 package com.example.biblioteca.app.shared.exceptions;
 
+import com.example.biblioteca.modules.accounts.domain.exceptions.InvalidPasswordFormat;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.ResponseEntity;
 
@@ -17,12 +18,37 @@ class GlobalExceptionHandlerTest {
         assertThat(result.getBody().message).isEqualTo("Invalid movie name");
     }
 
-
     @Test
     void invalidYearForMovie_shouldHandleInvalidYearForMovie() {
         ResponseEntity<ErrorDetails> result = sut.invalidYearForMovie();
 
         assertThat(result.getStatusCodeValue()).isEqualTo(422);
         assertThat(result.getBody().message).isEqualTo("Invalid movie year");
+    }
+
+    @Test
+    void emailAlreadyInUse_shouldHandleEmailAlreadyInUse() {
+        ResponseEntity<ErrorDetails> result = sut.emailAlreadyInUse();
+
+        assertThat(result.getStatusCodeValue()).isEqualTo(422);
+        assertThat(result.getBody().message).isEqualTo("Email is already in use");
+    }
+
+    @Test
+    void invalidEmailAddress_shouldHandleInvalidEmailAddress() {
+        ResponseEntity<ErrorDetails> result = sut.invalidEmailAddress();
+
+        assertThat(result.getStatusCodeValue()).isEqualTo(422);
+        assertThat(result.getBody().message).isEqualTo("Invalid email address");
+    }
+
+    @Test
+    void invalidPasswordFormat_shouldHandleInvalidPasswordFormat() {
+        InvalidPasswordFormat exception = new InvalidPasswordFormat();
+
+        ResponseEntity<ErrorDetails> result = sut.invalidPasswordFormat(exception);
+
+        assertThat(result.getStatusCodeValue()).isEqualTo(422);
+        assertThat(result.getBody().message).isEqualTo("Invalid password format: " + exception.getMessage());
     }
 }
