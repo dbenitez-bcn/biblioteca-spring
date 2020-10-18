e3af223ba12c2e29802fa688e430775308a0ec15
dbenitez-bcn <dbr.bcn@gmail.com>
2020-10-18 15:50:42 +0200

    adds SecurityConfigurer

---
+++ src/main/java/com/example/biblioteca/app/accounts/application/config/SecurityConfigurer.java
@@ -0,0 +1,26 @@
+package com.example.biblioteca.app.accounts.application.config;
+
+import com.example.biblioteca.modules.accounts.application.BibliotecaUserDetailsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
+    private final BibliotecaUserDetailsService userDetailsService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests().antMatchers("/v1/login", "/v1/register").permitAll()
+                .anyRequest().authenticated();
+    }
+}
