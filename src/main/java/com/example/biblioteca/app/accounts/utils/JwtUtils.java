d8a7204409577923073e22e8afe5aca515d5f0e1
dbenitez-bcn <dbr.bcn@gmail.com>
2020-10-18 16:28:57 +0200

    makes secret final

--- src/main/java/com/example/biblioteca/app/accounts/utils/JwtUtils.java
+++ src/main/java/com/example/biblioteca/app/accounts/utils/JwtUtils.java
@@ -14,7 +14,7 @@ import java.util.function.Function;
 @Service
 public class JwtUtils {
 
-    private String SECRET_KEY = "MY_SECRET";
+    private final String SECRET_KEY = "MY_SECRET";
 
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
