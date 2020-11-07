83f80775119bc431303c7348a515f3d87c833136
dbenitez-bcn <dbr.bcn@gmail.com>
2020-11-07 13:10:34 +0100

    adds rental MovieService bean

--- src/main/java/com/example/biblioteca/modules/rentals/application/MovieCreatedSubscriber.java
+++ src/main/java/com/example/biblioteca/modules/rentals/application/MovieCreatedSubscriber.java
@@ -3,11 +3,13 @@ package com.example.biblioteca.modules.rentals.application;
 import com.example.biblioteca.modules.shared.events.MovieCreatedEvent;
 import com.example.biblioteca.modules.shared.events.Subscriber;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class MovieCreatedSubscriber implements Subscriber<MovieCreatedEvent> {
+    @Qualifier("rental.movieService")
     private final MovieService movieService;
 
     @Override
--- src/main/java/com/example/biblioteca/modules/rentals/application/MovieService.java
+++ src/main/java/com/example/biblioteca/modules/rentals/application/MovieService.java
@@ -1,7 +1,10 @@
 package com.example.biblioteca.modules.rentals.application;
 
+import org.springframework.stereotype.Component;
+
 import java.util.UUID;
 
+@Component("rental.movieService")
 public class MovieService {
     public void create(UUID id, String name) {
 
