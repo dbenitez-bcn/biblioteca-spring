a6d47bc2d06343af0990d29a7265c1fea710b0c3
dbenitez-bcn <dbr.bcn@gmail.com>
2020-11-07 14:10:02 +0100

    adds sql migrations for movie info

---
+++ src/main/resources/db/migration/V9__Populate_movies_info_table.sql
@@ -0,0 +1,3 @@
+INSERT INTO movies_info (id, name) VALUES ('40e6215d-b5c6-4896-987c-f30f3678f608', 'Pulp fiction');
+INSERT INTO movies_info (id, name) VALUES ('6ecd8c99-4036-403d-bf84-cf8400f67836', 'Avatar');
+INSERT INTO movies_info (id, name) VALUES ('3f333df6-90a4-4fda-8dd3-9485d27cee36', 'Endgame');
