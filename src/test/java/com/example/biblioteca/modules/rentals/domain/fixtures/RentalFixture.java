5097161d729f00b7bd5405b91a36d2ddd2779529
dbenitez-bcn <dbr.bcn@gmail.com>
2020-11-07 18:47:18 +0100

    adds default movie to fixtures

--- src/test/java/com/example/biblioteca/modules/rentals/domain/fixtures/RentalFixture.java
+++ src/test/java/com/example/biblioteca/modules/rentals/domain/fixtures/RentalFixture.java
@@ -1,15 +1,22 @@
 package com.example.biblioteca.modules.rentals.domain.fixtures;
 
+import com.example.biblioteca.modules.rentals.domain.aggregates.Movie;
 import com.example.biblioteca.modules.rentals.domain.aggregates.Rental;
+
 import java.util.UUID;
 
 public class RentalFixture {
 
     public static final UUID USER_ID = UUID.randomUUID();
     public static final UUID MOVIE_ID = UUID.randomUUID();
+    public static final String MOVIE_NAME = "A movie name";
 
     public static Rental defaultRental() {
         return new Rental(MOVIE_ID, USER_ID);
+    }
+
+    public static Movie defaultMovie() {
+        return new Movie(MOVIE_ID, MOVIE_NAME);
     }
 
     public static Rental customRental(UUID movieId, UUID userId) {
